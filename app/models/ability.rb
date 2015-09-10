ca986b8ccdb07fd4f630fd18d37cd25779714f2c
DamSamNang <kcattor@gmail.com>
2015-09-10 20:33:49 +0700

    rake task create smiple users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,6 +6,7 @@ class Ability
     if user.admin?
       can :manage, :all
     else
+      can :manage, [Like, MovieHistory, Review, Comment, Request], user_id: user.id
       can :read, :all
     end
   end

bb62df94c09849bc1523b2c6762b8de53a32e743
DamSamNang <kcattor@gmail.com>
2015-11-02 18:40:35 +0700

    search movies

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
   def show
+    @q = Movie.ransack(params[:q])
+    @movie_searchs = @q.result(distinct: true).order(created_at: :DESC).page params[:page]
     @category = Category.find params[:id]
     @categories = Category.all.order(name: :ASC)
     @movies = @category.movies.order(created_at: :DESC).page params[:page]
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,26 @@
 class MoviesController < ApplicationController
+  before_action :search_movie
+
   def index
-    @movies = Movie.all.order(created_at: :DESC).page params[:page]
     @categories = Category.all.order name: :ASC
+    if params[:q].present?
+      @movie_searchs = @q.result(distinct: true).order(created_at: :DESC).page params[:page]
+      render layout: "category"
+    else
+      @movies = Movie.all.order(created_at: :DESC).page params[:page]
+    end
   end
 
   def show
     @categories = Category.all.order name: :ASC
     @movie = Movie.find params[:id]
     @movies = Movie.all.order(created_at: :DESC).limit(10)
+    @movie_categories = @movie.categories
     render layout: "movie"
+  end
+
+  private
+  def search_movie
+    @q = Movie.ransack params[:q]
   end
 end

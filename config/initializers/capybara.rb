3da0d3229eaf430dc05bbfb9cd426f82da925b84
samnangcattor <kcattor@gmail.com>
2016-07-19 14:30:37 +0700

    lazy reload image

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,6 +1,5 @@
 Capybara.configure do |config|
   config.run_server = false
-  config.default_wait_time = 30
   config.default_driver = :webkit
   config.javascript_driver = :webkit
   config.app_host = 'https://accounts.google.com'

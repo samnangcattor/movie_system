ef6fbfd24fbf14df073b9f53263e53e75abb34d2
samnangcattor <kcattor@gmail.com>
2016-09-08 14:17:02 +0700

    check ready file google drive

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830074427) do
+ActiveRecord::Schema.define(version: 20160908064359) do
 
   create_table "authenticates", force: :cascade do |t|
     t.string   "authenticate", limit: 255
@@ -34,11 +34,12 @@ ActiveRecord::Schema.define(version: 20160830074427) do
   end
 
   create_table "downloads", force: :cascade do |t|
-    t.integer  "status",     limit: 4
-    t.integer  "movie_id",   limit: 4
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-    t.integer  "torrent",    limit: 4
+    t.integer  "status",        limit: 4
+    t.integer  "movie_id",      limit: 4
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.integer  "torrent",       limit: 4
+    t.integer  "file_progress", limit: 4, default: 0
   end
 
   add_index "downloads", ["movie_id"], name: "index_downloads_on_movie_id", using: :btree
